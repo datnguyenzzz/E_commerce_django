3f13dda1b7a49984983662d75accb0d85b42b910
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-20 18:38:24 +0300

    add more field in table

--- e_store/models.py
+++ e_store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name 
@@ -12,6 +13,16 @@ class Product(models.Model):
     name = models.CharField(max_length=255) 
     author = models.CharField(max_length=255, default='admin') 
     description = models.TextField(blank=True) 
+    slug = models.SlugField(max_length=255)
+
+    price = models.DecimalField(max_digits=5, decimal_places=2) 
+    is_active = models.BooleanField(default=True) 
+    in_stock = models.BooleanField(default=True)
+
+    date_created = models.DateTimeField(auto_now_add=True) 
+    date_updated = models.DateTimeField(auto_now=True)
 
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
