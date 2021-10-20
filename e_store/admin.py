563741221be8f89b8a681cb672714d3720410c85
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-20 19:07:09 +0300

    admin register for monitor

--- e_store/admin.py
+++ e_store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug'] 
+
+@admin.register(Product) 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'slug', 'price',
+                    'in_stock', 'date_created', 'date_updated']
+    
+    list_filter = ['in_stock', 'is_active'] 
+    list_editable = ['price', 'in_stock']
 
 # Register your models here.
