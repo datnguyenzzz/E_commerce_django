af549e6ada5e8bdf6b5d6786e263c23e220b3a7d
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-20 18:31:12 +0300

    working with sqlite. Charfield created

--- e_store/models.py
+++ e_store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return self.name 
+
+class Product(models.Model):
+    name = models.CharField(max_length=255) 
+    author = models.CharField(max_length=255, default='admin') 
+    description = models.TextField(blank=True) 
+
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+
