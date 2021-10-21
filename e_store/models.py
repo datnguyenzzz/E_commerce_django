ac28d257a2f7f80de1408f50f04d7dab5c19abc2
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-21 17:25:14 +0300

    update FK to user creator

--- e_store/models.py
+++ e_store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -27,6 +28,8 @@ class Product(models.Model):
     date_updated = models.DateTimeField(auto_now=True)
 
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    
     objects = models.Manager() 
     products = ActiveProductManager()
 
