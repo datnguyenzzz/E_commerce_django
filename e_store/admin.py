4a368b2cc2f5606984dcc447c19c47b5c622f1f8
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-21 18:37:54 +0300

    save file uploaded

--- e_store/admin.py
+++ e_store/admin.py
@@ -4,6 +4,7 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug'] 
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product) 
 class ProductAdmin(admin.ModelAdmin):
@@ -12,5 +13,6 @@ class ProductAdmin(admin.ModelAdmin):
     
     list_filter = ['in_stock', 'is_active'] 
     list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
