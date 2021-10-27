8fa86f4817310f3313ab78ad59506f4887353eb7
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-27 22:34:07 +0300

    action when add user

--- account/models.py
+++ account/models.py
@@ -4,7 +4,30 @@ from django_countries.fields import CountryField
 from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
-    pass
+    def create_user(self, email, user_name, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email'))  
+        
+        email = self.normalize_email(email) 
+        user = self.model(email=email, user_name = user_name, **other_fields) 
+
+        user.set_password(password)
+        user.save() 
+        return user
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True) 
+        other_fields.setdefault('is_superuser', True) 
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to "is_staff=True".')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to "is_superuser=True".')
+
+        return self.create_user(email, user_name, password, **other_fields)
 
 # Create your models here.
 class UserBase(AbstractBaseUser, PermissionsMixin): 
