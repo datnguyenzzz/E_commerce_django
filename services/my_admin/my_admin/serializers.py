83192f1ccea3d1d690a0c824c3c62f3faf34676d
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-29 22:43:54 +0300

    category serializer

--- services/my_admin/my_admin/serializers.py
+++ services/my_admin/my_admin/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers 
 
-from .models import Products 
+from .models import Products,Category 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
