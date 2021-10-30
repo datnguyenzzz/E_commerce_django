00cc75061447294a7ca06ad1b4248d7077572a2a
datnguyen79198 <datnguyen79198@gmail.com>
2021-10-30 15:57:52 +0300

    user get api

--- services/user_view/products/views.py
+++ services/user_view/products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets 
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Products
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ViewSet):
+    def get_all(self, request):
+        products = Products.products.all() 
+        serializer = ProductSerializer(products, many=True) 
+        return Response(serializer.data)
+
+    def get_wid(self, request, id):
+        product = Products.products.get(id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
