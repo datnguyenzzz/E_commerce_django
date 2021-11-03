3da32e6a952717d2fb1a6200c7dcb496eb7e790c
datnguyen79198 <datnguyen79198@gmail.com>
2021-11-03 17:31:05 +0300

    init server listen to endpoint api

--- services/payment_system_1/stream_request_service/app/src/main/java/stream_request_service/App.java
+++ services/payment_system_1/stream_request_service/app/src/main/java/stream_request_service/App.java
@@ -3,12 +3,46 @@
  */
 package stream_request_service;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.http.javadsl.Http; 
+import akka.http.javadsl.ServerBinding; 
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+
+import java.util.concurrent.CompletionStage;
+import java.io.IOException;
+
+public class App extends AllDirectives{
+
+    private Route createRoute() {
+        return concat(
+            path("api", () -> 
+                get(() -> complete("This is server for api....")))
+        );
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(),"stream-request-server");
+        
+        final Http http = Http.get(system);
+        App app = new App(); 
+
+        final CompletionStage<ServerBinding> binding = 
+            http.newServerAt("localhost",8000)
+                .bind(app.createRoute());
+
+        System.out.println("Server is online");
+        try {
+            System.in.read();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        binding
+            .thenCompose(ServerBinding::unbind)
+            .thenAccept(unbound -> system.terminate());
+
     }
 }
