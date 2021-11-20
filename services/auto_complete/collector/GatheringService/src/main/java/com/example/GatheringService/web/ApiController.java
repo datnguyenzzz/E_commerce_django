5c44688e14de3b8d34ae76c98e5e87af9ceb6570
datnguyen79198 <datnguyen79198@gmail.com>
2021-11-20 15:56:59 +0300

    update dependencies for hadoop

--- services/auto_complete/collector/GatheringService/src/main/java/com/example/GatheringService/web/ApiController.java
+++ services/auto_complete/collector/GatheringService/src/main/java/com/example/GatheringService/web/ApiController.java
@@ -3,10 +3,12 @@ package com.example.GatheringService.web;
 import com.example.GatheringService.dto.GatherRequest;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
@@ -25,6 +27,7 @@ public class ApiController {
 
     @PostMapping(value = "/gather",
                  produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
     public String handlePostRequest(@RequestBody GatherRequest request) {
         logger.info("Gathering request: " + request.toString());
         return request.toString();
