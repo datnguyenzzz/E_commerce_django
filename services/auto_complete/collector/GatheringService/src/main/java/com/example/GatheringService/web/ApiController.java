76b89e8a67dad9148aab70e12b147800c52c0d2d
datnguyen79198 <datnguyen79198@gmail.com>
2021-11-20 12:25:00 +0300

    log configuration

--- services/auto_complete/collector/GatheringService/src/main/java/com/example/GatheringService/web/ApiController.java
+++ services/auto_complete/collector/GatheringService/src/main/java/com/example/GatheringService/web/ApiController.java
@@ -2,16 +2,21 @@ package com.example.GatheringService.web;
 
 import com.example.GatheringService.dto.GatherRequest;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+
 @RestController
 @RequestMapping(value = "/api/v1.0",
                 produces = "application/json")
 public class ApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
     
     @GetMapping("/version")
     public String getVersion() {
@@ -21,6 +26,7 @@ public class ApiController {
     @PostMapping(value = "/gather",
                  produces = "application/json")
     public String handlePostRequest(@RequestBody GatherRequest request) {
+        logger.info("Gathering request: " + request.toString());
         return request.toString();
     }
 }
