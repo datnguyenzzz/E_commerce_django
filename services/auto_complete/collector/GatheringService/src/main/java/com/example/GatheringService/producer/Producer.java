79a22895c88a42c7e4ae319569e0d19e88e7adff
datnguyen79198 <datnguyen79198@gmail.com>
2021-11-22 17:48:35 +0300

    FI vs CI

--- services/auto_complete/collector/GatheringService/src/main/java/com/example/GatheringService/producer/Producer.java
+++ services/auto_complete/collector/GatheringService/src/main/java/com/example/GatheringService/producer/Producer.java
@@ -7,19 +7,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+// field injection vs contructor injection 
+// FI accept all value -> Some value might be unprocessable
+// CI only accept when provided wired field 
+
 @Service
 public class Producer {
 
     @Value("${topic.name}")
     private String topicName;
 
+    //@Autowired can't wire final like this
     private final KafkaTemplate<String, GatherRequest> kafkaTemplate;
 
     @Autowired
     public Producer(KafkaTemplate<String, GatherRequest> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
-
+    
     public void sendEvent(GatherRequest gatherRequest) {
         this.kafkaTemplate.send(this.topicName, gatherRequest);
     }
