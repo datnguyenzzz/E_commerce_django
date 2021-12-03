3031b71631a601d4251877156bbb585f888aa85b
datnguyen79198 <datnguyen79198@gmail.com>
2021-12-03 17:43:53 +0300

    bulk request

--- services/auto_complete/bulk-request.sh
+++ services/auto_complete/bulk-request.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-curl -X POST http://localhost:8080/api/v1.0/gather\
-     -H 'Content-Type: application/json'\
-     -d '{"word":"dfhjgasdhjgasdkj","lang":"en"}'
+for i in {1..10} 
+do
+    NUMBER=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes 2)
+    phrase=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w $NUMBER | head -n 1)
+    TIME=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes 1)
+    for (( c=1; c<=$TIME; c++ ))
+    do
+        curl -X POST http://localhost:8080/api/v1.0/gather\
+        -H 'Content-Type: application/json'\
+        -d '{"word":"'$phrase'","lang":"en"}'
+    done
+
+done
