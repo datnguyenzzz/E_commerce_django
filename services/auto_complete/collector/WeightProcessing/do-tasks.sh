35d1f5d0b2c53c21829d0fa0bc5b73eb6e6db2bb
datnguyen79198 <datnguyen79198@gmail.com>
2021-12-07 10:54:53 +0300

    testing input hdfs batch

--- services/auto_complete/collector/WeightProcessing/do-tasks.sh
+++ services/auto_complete/collector/WeightProcessing/do-tasks.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-hadoop fs -ls /words
+TARGET_ID=$(date +%Y%m%d_%H%M)
+
+echo "Checking if /words/kafka_sink/words-from-client/ exists.."
+while ! $(hadoop fs -test -d "/words/kafka_sink/words-from-client/") ; do \
+    echo "Waiting for folder /words/kafka_sink/words-from-client/ to be created by kafka connect. Please wait.."; 
+done
+
+echo "Checking for contents in /words/kafka_sink/words-from-client/.."
+while [[ $(hadoop fs -ls /words/kafka_sink/words-from-client/ | sed 1,1d) == "" ]] ; do \
+    echo "Waiting for kafka connect to populate/words/kafka_sink/words-from-client/*. Please wait.."; 
+done
+
+# CREATE WEIGHT HDFS BATCH
+hadoop fs -mkdir -p /words/2_with_weight/${TARGET_ID}/
 
 echo "STARTING ADD-WEIGHT SERVICE ...."
 cd /HADOOP_TASKS/adding-words-weight
