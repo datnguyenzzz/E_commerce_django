dce7ada6f3243dabe7c1dea1135a987e61136a24
datnguyen79198 <datnguyen79198@gmail.com>
2021-12-07 10:37:22 +0300

    add maven to hadoop base image

--- services/auto_complete/collector/WeightProcessing/do-tasks.sh
+++ services/auto_complete/collector/WeightProcessing/do-tasks.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 hadoop fs -ls /words
+
+echo "STARTING ADD-WEIGHT SERVICE ...."
+cd /HADOOP_TASKS/adding-words-weight
+mvn package
+java -cp target/adding-words-weight-1.0-SNAPSHOT.jar com.mycompany.app.App
