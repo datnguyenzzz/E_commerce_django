9709614d768665c42965758a51992923881290f8
datnguyen79198 <datnguyen79198@gmail.com>
2021-12-27 14:16:30 +0300

    get results from redis

--- services/auto_complete/distributor/recommend/service.py
+++ services/auto_complete/distributor/recommend/service.py
@@ -1,5 +1,6 @@
 from kazoo.client import KazooClient
 import redis
+import pickle
 from MyLogger import MyLogger
 
 ZK_LAST_BUILT_TARGET = f'/autocomplete/distributor/last_built_target'
@@ -12,9 +13,6 @@ class WordRecommendation:
         self._zk = KazooClient(hosts=f'{os.getenv("ZK_HOST")}:{os.getenv("ZK_PORT")}')
         #cache 
         self._cache = redis.Redis(host=os.getenv("CACHE_HOST"), port=os.getenv("CACHE_PORT"), db=0)
-        self._distributed_cache_enabled = True \
-            if os.getenv("DISTRIBUTED_CACHE_ENABLED") == 'true' \
-            else False
           
     def start(self):
         self._zk.start()
@@ -23,4 +21,20 @@ class WordRecommendation:
         self._zk.stop()
         
     def top_phrases_for(self, phrase):
+        top_phrases = self._top_phrases_from_cache(phrase)
+        if top_phrases is not None:
+            self._logger.info(f'Got top phrases from cache: {top_phrases}')
+            return top_phrases
+        
         return [phrase]
+    
+    def top_phrases_from_cache(self, phrase):
+        cache_key = phrase 
+        time_expire = int(os.getenv("CACHE_LIFE_TIME")) * 60 
+        self._logger.info(f'Get top phrases from cache with key: {cache_key}')
+        
+        if self._cache.exists(key):
+            pickled_list = self._cache.get(key)
+            return pickle.loads(pickled_list)
+        
+        return None
