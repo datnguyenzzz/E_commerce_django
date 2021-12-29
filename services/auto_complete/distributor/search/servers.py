e13b42db458a4f8c1757c29fe9508c29c13b494b
datnguyen79198 <datnguyen79198@gmail.com>
2021-12-29 11:14:25 +0300

    connect to zk search service

--- services/auto_complete/distributor/search/servers.py
+++ services/auto_complete/distributor/search/servers.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 from kazoo.client import KazooClient, DataWatch
 
 from MyLogger import MyLogger
@@ -7,10 +8,23 @@ from HdfsClient import HdfsClient
 LAST_BUILT_TARGET = '/autocomplete/distributor/last_built_target'
 TRIE_PARTITION = os.getenv("TRIE_PARTITION")
 
-class Servers:
-    def __init__(self, partition):
+def _init_boundaries():
+    all_size = ord('z') - ord('a') + 1 
+    partitions_size = all_size // TRIE_PARTITIONS + 1
         
-        self._partition = partition
+    boundaries = [] 
+    for i in range(ord('a'),ord('z')+1, partitions_size):
+        if i!= ord('a'):
+            boundaries.append(chr(i))
+            
+    boundaries.append('{') 
+        
+    return boundaries  
+
+class Servers:
+    _partitions = _init_boundaries()
+    def __init__(self, partition):
+        self._partition = self._partitions[partition]
         #logger
         self._logger = MyLogger(__name__).logger
         #hdfs client 
@@ -21,6 +35,12 @@ class Servers:
     def start(self):
         self._zk.start()
         _ = DataWatch(client = self._zk, path=LAST_BUILT_TARGET, func=self._on_last_built_target_changed)
+    
+    def _load_trie(self, target_id):
+        pass
 
     def _on_last_built_target_changed(self, data, stat, event=None):
-        pass
+        self._logger.info(f'last built target changed: {data}')
+        if data is None:
+            return
+        target_id = data.decode()
