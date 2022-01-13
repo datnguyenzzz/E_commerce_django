d414d0f41651bf98005046119587d474c259fc8e
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-13 16:28:40 +0300

    roll back

--- services/recommender_service/rating-command-service/src/main/java/vn/datnguyen/recommender/Backend/Rating.java
+++
@@ -1,8 +0,0 @@
-package vn.datnguyen.recommender.Backend;
-
-import io.eventuate.ReflectiveMutableCommandProcessingAggregate;
-
-//@Service
-public class Rating extends ReflectiveMutableCommandProcessingAggregate<Rating, RatingCommand> {
-    private boolean isDeleted;
-}
--- services/recommender_service/rating-command-service/src/main/java/vn/datnguyen/recommender/Backend/RatingCommand.java
+++
@@ -1,7 +0,0 @@
-package vn.datnguyen.recommender.Backend;
-
-import io.eventuate.Command;
-
-interface RatingCommand extends Command {
-    
-}
