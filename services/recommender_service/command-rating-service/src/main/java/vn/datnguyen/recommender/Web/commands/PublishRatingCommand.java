f94ae3a3774397a2726015494ad25e2cc24ea534
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-14 15:07:11 +0300

    publish rating command

---
+++ services/recommender_service/command-rating-service/src/main/java/vn/datnguyen/recommender/Web/commands/PublishRatingCommand.java
@@ -0,0 +1,46 @@
+package vn.datnguyen.recommender.Web.commands;
+
+import java.util.Locale;
+
+public class PublishRatingCommand {
+    private String clientId; 
+    private String itemId; 
+    private int score; 
+
+    public PublishRatingCommand(String clientId, String itemId, int score) {
+        this.clientId = clientId; 
+        this.itemId = itemId; 
+        this.score = score;
+    }
+
+    public String getClientId() {
+        return this.clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getItemId() {
+        return this.itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(), 
+                            "PublishRatingCommand(clientId=%s,itemId=%s,score=%d)",
+                            getClientId(), getItemId(), getScore());
+    }
+}
