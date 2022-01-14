8d0bab6ff86babc06c3ed777e9102d95a321dac1
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-14 15:25:23 +0300

    logger

--- services/recommender_service/command-rating-service/src/main/java/vn/datnguyen/recommender/Web/ApiController.java
+++ services/recommender_service/command-rating-service/src/main/java/vn/datnguyen/recommender/Web/ApiController.java
@@ -1,5 +1,7 @@
 package vn.datnguyen.recommender.Web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v/1.0.0/rating")
 public class ApiController {
+
+    private final Logger logger = LoggerFactory.getLogger(ApiController.class);
     
     @GetMapping("/test")
     public String testApi() {
+        logger.info("COMMAND-RATING-SERVICE" + "testing service");
         return "v.1.0.0";
     }
 }
