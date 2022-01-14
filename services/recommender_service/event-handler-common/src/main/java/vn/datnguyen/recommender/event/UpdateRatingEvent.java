9054dad04f1953fb6fb434e5734cc18a1863d045
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-14 14:09:36 +0300

    update rating event

---
+++ services/recommender_service/event-handler-common/src/main/java/vn/datnguyen/recommender/event/UpdateRatingEvent.java
@@ -0,0 +1,35 @@
+package vn.datnguyen.recommender.event;
+
+import java.util.Locale;
+
+public class UpdateRatingEvent extends Event {
+    private final String clientId; 
+    private final String itemId; 
+    private final int score;
+
+    public UpdateRatingEvent(final String clientId, final String itemId, final int score) {
+        super();
+        this.clientId = clientId; 
+        this.itemId = itemId; 
+        this.score = score;
+    }
+
+    public String getClientId() {
+        return this.clientId;
+    }
+
+    public String getItemId() {
+        return this.itemId;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(), 
+                "PublishRatingEvent{eventId=%s, clientId=%s, itemId=%s, score=%d}",
+                this.getEventId(), this.clientId, this.itemId, this.score);
+    }
+}
