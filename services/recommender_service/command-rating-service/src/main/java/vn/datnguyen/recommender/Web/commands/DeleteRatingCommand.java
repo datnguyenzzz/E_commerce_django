ba36d1065c411498f4a3670b8adcb895bf5fe05e
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-14 15:18:11 +0300

    delete rating service

---
+++ services/recommender_service/command-rating-service/src/main/java/vn/datnguyen/recommender/Web/commands/DeleteRatingCommand.java
@@ -0,0 +1,36 @@
+package vn.datnguyen.recommender.Web.commands;
+
+import java.util.Locale;
+
+public class DeleteRatingCommand {
+    private String clientId; 
+    private String itemId; 
+
+    public DeleteRatingCommand(String clientId, String itemId) {
+        this.clientId = clientId; 
+        this.itemId = itemId; 
+    }
+
+    public String getClientId() {
+        return this.clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getItemId() {
+        return this.itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(), 
+                            "DeleteRatingCommand(clientId=%s,itemId=%s)",
+                            getClientId(), getItemId());
+    }
+}
