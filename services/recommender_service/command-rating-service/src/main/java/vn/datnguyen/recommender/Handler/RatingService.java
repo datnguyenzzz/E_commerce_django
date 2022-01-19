a1e379840379fdfec668a3c3c5e4087527ad6f8f
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-19 14:36:11 +0300

    error rating command

--- services/recommender_service/command-rating-service/src/main/java/vn/datnguyen/recommender/Handler/RatingService.java
+++ services/recommender_service/command-rating-service/src/main/java/vn/datnguyen/recommender/Handler/RatingService.java
@@ -7,20 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import vn.datnguyen.recommender.AvroClasses.AvroEvent;
 import vn.datnguyen.recommender.Domain.Command;
 import vn.datnguyen.recommender.Domain.DeleteRatingCommand;
-import vn.datnguyen.recommender.Domain.DeleteRatingEvent;
-import vn.datnguyen.recommender.Domain.Event;
 import vn.datnguyen.recommender.Domain.PublishRatingCommand;
-import vn.datnguyen.recommender.Domain.PublishRatingEvent;
 import vn.datnguyen.recommender.Domain.UpdateRatingCommand;
-import vn.datnguyen.recommender.Domain.UpdateRatingEvent;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 
 @Service
-public class RatingService implements CommandHandler {
+public class RatingService implements CommandHandler, EventHandler {
 
     private RatingTransactionalPublisher ratingPublisher;
 
@@ -37,7 +34,7 @@ public class RatingService implements CommandHandler {
         return CompletableFuture.runAsync(() -> validate(command).forEach(ratingPublisher::execute));
     }
 
-    private List<Event> validate(Command command) {
+    private List<AvroEvent> validate(Command command) {
         if (command instanceof PublishRatingCommand) {
             return validate((PublishRatingCommand) command);
         }
@@ -50,29 +47,60 @@ public class RatingService implements CommandHandler {
         return emptyList();
     }
 
-    private List<Event> validate(PublishRatingCommand command) {
-        int messageId = Integer.parseInt(partitionId);
-        return singletonList(new PublishRatingEvent(command.getClientId(), command.getItemId(), command.getScore(), messageId));
+    private List<AvroEvent> toAvroEvent(Command command) {
+        if (command instanceof PublishRatingCommand) {
+            return toAvroEvent((PublishRatingCommand) command);
+        }
+        else if (command instanceof UpdateRatingCommand) {
+            return toAvroEvent((UpdateRatingCommand) command);
+        }
+        else {
+            return toAvroEvent((DeleteRatingCommand) command);
+        }
     }
 
-    private List<Event> validate(UpdateRatingCommand command) {
-        int messageId = Integer.parseInt(partitionId);
-        return singletonList(new UpdateRatingEvent(command.getClientId(), command.getItemId(), command.getScore(), messageId));
+    private Command validate(PublishRatingCommand command) {
+        //int messageId = Integer.parseInt(partitionId);
+        boolean acceptable = true;
+
+        if (acceptable) {
+            return command;
+        }
+        
+        return emptyList();
     }
 
-    private List<Event> validate(DeleteRatingCommand command) {
-        int messageId = Integer.parseInt(partitionId);
-        return singletonList(new DeleteRatingEvent(command.getClientId(), command.getItemId(), messageId));
+    private Command validate(UpdateRatingCommand command) {
+        boolean acceptable = true;
+        
+        if (acceptable) {
+            return toAvroEvent(command);
+        }
+        
+        return emptyList();
     }
+
+    private Command validate(DeleteRatingCommand command) {
+        boolean acceptable = true;
+        
+        if (acceptable) {
+            return toAvroEvent(command);
+        }
+        
+        return emptyList();
+    }
+
+    private 
+
 
     @Override
-    public CompletableFuture<Void> process(Event event) {
+    public CompletableFuture<Void> process(AvroEvent event) {
         // For saga invoked
         return null;
     }
 
     @Override
-    public void apply(Event event) {
+    public void apply(AvroEvent event) {
         //apply changes to aggregate
     }
 }
