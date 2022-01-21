368efaa7bfd43bdd05ae48bf37c6a886e93a1730
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-21 16:36:19 +0300

    event entity

--- services/recommender_service/event-source-storage/src/main/java/vn/datnguyen/recommender/Models/EventEntity.java
+++ services/recommender_service/event-source-storage/src/main/java/vn/datnguyen/recommender/Models/EventEntity.java
@@ -1,14 +1,23 @@
 package vn.datnguyen.recommender.Models;
 
+import java.io.IOException;
+import java.util.Map;
+
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
+
+import vn.datnguyen.recommender.Utils.HashMapConverter;
 
 
 @Entity
@@ -23,7 +32,58 @@ public class EventEntity {
     @Column(name = "TYPE")
     private String eventType;
 
-    @Type(type = "jsonb")
-    @Column(columnDefinition = "jsonb")
-    private PayloadEntity payload;
+    @Column(name = "PAYLOAD")
+    private String payloadJSON;
+    
+    // Object -> String before persist 
+    // String -> Object when get
+    @Convert(converter = HashMapConverter.class)
+    private Map<String, Object> payload;
+
+    public long getEventId() {
+        return this.eventId;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventType() {
+        return this.eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getPayloadJSON() {
+        return this.payloadJSON;
+    }
+
+    public void setPayloadJSON(String payloadJSON) {
+        this.payloadJSON = payloadJSON;
+    }
+
+    public Map<String, Object> getPayload() {
+        return this.payload;
+    }
+
+    public void setPayload(Map<String, Object> payload) {
+        this.payload = payload;
+    }
+
+    // Object mapper 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    
+    public void serializePayload() throws JsonProcessingException {
+        String jsonResult = objectMapper.writeValueAsString(payload);
+        setPayloadJSON(jsonResult);
+    }
+
+    public void deserializePayload() throws IOException {
+        Map<String, Object> mapResult = 
+            objectMapper.readValue(payloadJSON, new TypeReference<Map<String, Object>>(){});
+        
+        setPayload(mapResult);
+    }
 }
---
+++ services/recommender_service/event-source-storage/src/main/java/vn/datnguyen/recommender/Utils/HashMapConverter.java
@@ -0,0 +1,5 @@
+package vn.datnguyen.recommender.Utils;
+
+public class HashMapConverter {
+    
+}
