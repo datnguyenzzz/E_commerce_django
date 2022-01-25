7828d92ff415fd28a3dcd2b59a4eefdf2c22e660
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-25 15:40:30 +0300

    publish event to query event

--- services/recommender_service/transaction-log-tailling/src/main/java/vn/datnguyen/recommender/Handler/TransactionalPublisher.java
+++ services/recommender_service/transaction-log-tailling/src/main/java/vn/datnguyen/recommender/Handler/TransactionalPublisher.java
@@ -3,6 +3,10 @@ package vn.datnguyen.recommender.Handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import vn.datnguyen.recommender.AvroClasses.AvroEvent;
@@ -11,12 +15,40 @@ import vn.datnguyen.recommender.MessageQueue.Publisher;
 @Component
 public class TransactionalPublisher implements Publisher {
 
+    @Value("${transactionKafka.topicToQueryService}")
+    private String topicToQueryService;
+
+    @Value("${transactionKafka.topicToRecommendSerivce}")
+    private String topicToRecommendSerivce;
+
     private final Logger logger = LoggerFactory.getLogger(TransactionalPublisher.class);
+    private KafkaTemplate<String, AvroEvent> kafkaTemplate;
     
-    public TransactionalPublisher() {}
+    @Autowired
+    public TransactionalPublisher(KafkaTemplate<String, AvroEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @Override
     public void execute(AvroEvent event) {
         logger.info("EVENT-SOURCE: Transaction publish event " + event);
+
+        kafkaTemplate.executeInTransaction(op -> {
+            op.send(topicToQueryService, Integer.toString(event.getPartitionId()), event)
+                .addCallback(this::onSuccessToQueryService, this::onFailureToQueryService);
+            
+            // send to recommendation service
+            // op.send();
+            return true;
+        });
+    }
+
+    private void onSuccessToQueryService(final SendResult<String, AvroEvent> res) {
+        logger.info("EVENT-SOURCE: Sucessfully publish event to QUERY-RATING-SERVICE = " 
+                    + res.getProducerRecord().toString());
+    }
+
+    private void onFailureToQueryService(final Throwable t) {
+        logger.warn("EVENT-SOURCE: Unable publish event to QUERY-RATING-SERVICE =" + t.getMessage());
     }
 }
