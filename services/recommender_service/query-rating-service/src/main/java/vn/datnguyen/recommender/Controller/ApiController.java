c992be605cefd0c18eb89eda958dfee6212c48bf
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-28 11:38:35 +0300

    get record directly from db

--- services/recommender_service/query-rating-service/src/main/java/vn/datnguyen/recommender/Controller/ApiController.java
+++ services/recommender_service/query-rating-service/src/main/java/vn/datnguyen/recommender/Controller/ApiController.java
@@ -1,5 +1,8 @@
 package vn.datnguyen.recommender.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,23 +10,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.datnguyen.recommender.Models.Rating;
+import vn.datnguyen.recommender.Repositories.RatingRepository;
+
 @RestController
 @RequestMapping("/api/v/1.0.0/rating")
 public class ApiController {
+
+    private RatingRepository ratingRepository;
+
+    @Autowired
+    public ApiController(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
     
     @GetMapping
-    public ResponseEntity<String> getRating(@RequestParam(required = false) String clientId, @RequestParam(required = false) String itemId) {
+    public ResponseEntity<List<Rating>> getRating(@RequestParam(required = false) String clientId, @RequestParam(required = false) String itemId) {
         if (clientId != null && itemId != null) {
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body("HAS BOTH CLIENTID + ITEMID");
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(ratingRepository.findByClientIdAndItemId(clientId, itemId));
         }
         else if (clientId==null && itemId==null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("REQUIRE AT LEAST CLIENTID OR ITEMID");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         else if (clientId != null) {
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body("HAS ONLY CLIENTID");
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(ratingRepository.findByClientId(clientId));
         }
         else {
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body("HAS ONLY ITEMID");
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(ratingRepository.findByItemId(itemId));
         }
     }
 
