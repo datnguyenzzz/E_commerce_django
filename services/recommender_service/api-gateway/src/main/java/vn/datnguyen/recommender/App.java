864bb3bb1bcfd4b32ca1325d5cb22a2be69cf20f
datnguyen79198 <datnguyen79198@gmail.com>
2022-01-30 14:24:21 +0300

    register gateway to eureka

--- services/recommender_service/api-gateway/src/main/java/vn/datnguyen/recommender/App.java
+++ services/recommender_service/api-gateway/src/main/java/vn/datnguyen/recommender/App.java
@@ -1,13 +1,18 @@
 package vn.datnguyen.recommender;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+@SpringBootApplication
+@EnableEurekaClient
+public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
