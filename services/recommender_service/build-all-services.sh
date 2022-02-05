c9b12921571742e1e4fc7b59219e64421415cf5e
datnguyen79198 <datnguyen79198@gmail.com>
2022-02-05 14:11:34 +0300

    add example storm topology

--- services/recommender_service/build-all-services.sh
+++ services/recommender_service/build-all-services.sh
@@ -1,4 +1,10 @@
 #! /bin/bash 
 
-mvn clean install
-docker-compose up -d
+docker-compose up -d zookeeper nimbus supervisor ui --scale supervisor=3
+docker build -t wurstmeister/storm ./storm/storm-base
+docker build -t wurstmeister/storm-nimbus ./storm/storm-nimbus
+docker build -t wurstmeister/storm-supervisor ./storm/storm-supervisor
+docker build -t wurstmeister/storm-ui ./storm/storm-ui
+containerId=$(docker inspect -f '{{.Id}}' recommender_service-nimbus-1)
+docker cp ./recommender-real-time-service/target/recommender-real-time-service-1.0-SNAPSHOT.jar \
+        ${containerId}:/storm-topology/topo.jar
---
+++ services/recommender_service/recommender-real-time-service/src/main/java/vn/datnguyen/recommender/App.java
@@ -0,0 +1,63 @@
+package vn.datnguyen.recommender;
+
+import java.util.Map;
+
+import org.apache.storm.Config;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.testing.TestWordSpout;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+public class App {
+    public static class ExampleBolt extends BaseRichBolt {
+
+        private OutputCollector collector;
+    
+        @Override
+        public void prepare(Map map, TopologyContext TopologyContext, OutputCollector collector) {
+            this.collector = collector;
+        }
+    
+        @Override
+        public void execute(Tuple tuple) {
+            String word = tuple.getString(0); 
+    
+            StringBuilder sb = new StringBuilder();
+            sb.append(word).append(">_<");
+    
+            collector.emit(tuple, new Values(sb.toString()));
+            collector.ack(tuple);
+        }
+    
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("test-output-bolt"));
+        }
+    
+    }
+    public static void main( String[] args ) throws Exception {
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+
+        topologyBuilder.setSpout("mew-test-spout", new TestWordSpout(), 1);
+
+        topologyBuilder.setBolt("bolt-test-1", new ExampleBolt(), 3)
+            .shuffleGrouping("mew-test-spout");
+        
+        topologyBuilder.setBolt("bolt-test-2", new ExampleBolt(), 2)
+            .shuffleGrouping("bolt-test-1");
+
+        Config config = new Config();
+        config.setDebug(true);
+        //3 supervisor - 5 worker each
+        config.setNumWorkers(5);
+
+        StormSubmitter.submitTopology("Example-Topology", config, topologyBuilder.createTopology());
+
+    }
+}
