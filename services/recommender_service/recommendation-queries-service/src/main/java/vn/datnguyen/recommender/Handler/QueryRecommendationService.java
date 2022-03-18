f6e01972b3e6f676c9b19f127e5520fb9b297b72
datnguyen79198 <datnguyen79198@gmail.com>
2022-03-18 23:11:47 +0300

    convert command to avro event

--- services/recommender_service/recommendation-queries-service/src/main/java/vn/datnguyen/recommender/Handler/QueryRecommendationService.java
+++ services/recommender_service/recommendation-queries-service/src/main/java/vn/datnguyen/recommender/Handler/QueryRecommendationService.java
@@ -1,13 +1,21 @@
 package vn.datnguyen.recommender.Handler;
 
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import vn.datnguyen.recommender.AvroClasses.AvroEvent;
+import vn.datnguyen.recommender.AvroClasses.AvroRecommendForItem;
 import vn.datnguyen.recommender.Domain.Command;
+import vn.datnguyen.recommender.Domain.RecommendationForItemCommand;
 
 @Service
 public class QueryRecommendationService implements CommandHandler {
+
+    @Value("${incomingEvent.avroRecommendForItemEvent}")
+    private String avroRecommendForItemEvent;
     
     public QueryRecommendationService() {}
 
@@ -15,8 +23,39 @@ public class QueryRecommendationService implements CommandHandler {
     public CompletableFuture<Object> process(Command command) {
         return CompletableFuture.supplyAsync(
             () -> {
-                return command.toString();
+                AvroEvent event = toAvroEvent(command);
+                return event.toString();
             }
         );
+    }
+
+    private AvroEvent toAvroEvent(Command command) {
+        if (command instanceof RecommendationForItemCommand) {
+            return toAvroEvent((RecommendationForItemCommand) command);
+        }
+        return null;
+    }
+
+    private AvroEvent toAvroEvent(RecommendationForItemCommand command) {
+        AvroRecommendForItem eventPayload = AvroRecommendForItem.newBuilder()
+            .setItemId(command.getItemId())
+            .setLimit(command.getLimit())
+            .setProperties1(command.getProperty1())
+            .setProperties2(command.getProperty2())
+            .setProperties3(command.getProperty3())
+            .build();
+        
+        return wrap(eventPayload, avroRecommendForItemEvent);
+    }
+
+    private AvroEvent wrap(Object payload, String payloadType) {
+
+        return AvroEvent.newBuilder()
+                        .setEventId(UUID.randomUUID().toString())
+                        .setPartitionId(-1)
+                        .setTimestamp(Long.toString(System.currentTimeMillis()))
+                        .setEventType(payloadType)
+                        .setData(payload)
+                        .build();
     }
 }
